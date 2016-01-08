3ad00ab1858fc20b8463366ffdc2915f95db2291
Manish Goyal <manish.goyal@mobilelive.ca>
2016-01-07 21:33:13 -0500

    adding files

---
+++ src/test/java/com/mycompany/myapp/web/rest/Swagger2MarkupIntTest.java
@@ -0,0 +1,48 @@
+package com.mycompany.myapp.web.rest;
+
+import com.mycompany.myapp.Application;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.IntegrationTest;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+import springfox.documentation.staticdocs.SwaggerResultHandler;
+
+import javax.inject.Inject;
+import java.io.File;
+import java.io.IOException;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = Application.class)
+@WebAppConfiguration
+@IntegrationTest
+public class Swagger2MarkupIntTest {
+
+    @Inject
+    private WebApplicationContext context;
+
+    private MockMvc mockMvc;
+
+    @Before
+    public void setup() throws IOException {
+        this.mockMvc = MockMvcBuilders.webAppContextSetup(this.context).build();
+    }
+
+    @Test
+    public void convertSwaggerToAsciiDoc() throws Exception {
+        this.mockMvc.perform(get("/v2/api-docs")
+            .accept(MediaType.APPLICATION_JSON))
+            .andDo(SwaggerResultHandler.outputDirectory("target/swagger")            .build())
+            .andExpect(status().isOk());
+    }
+}
